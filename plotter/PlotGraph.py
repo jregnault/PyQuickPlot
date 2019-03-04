68e156e1e08fd432e9c1e5812e18a4060a58c6c2
Jérémie REGNAULT <jeremie.regnault.etu@univ-lille.fr>
2019-03-04 11:02:51 +0100

    feat : add title option
    allow to change the figure's title

--- plotter/PlotGraph.py
+++ plotter/PlotGraph.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 
 class PlotGraph:
 
-    def __init__(self, title="plot", x_label="", y_label=""):
+    def __init__(self, title="plot", x_label="x", y_label="y"):
         self.title = title
         self.x_label = x_label
         self.y_label = y_label
 
     def draw(self, data, format="bo-"):
-        plt.title = self.title
         plt.plot(data, format)
+        plt.title(self.title)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
         plt.show()
