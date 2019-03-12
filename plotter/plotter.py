f321269d448f7abaf8398561750039850dec11ef
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-03-12 17:02:07 +0100

    doc : add verbose messages

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -4,9 +4,16 @@ import PlotGraph
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a figure from raw data.")
 
+    # General options
     parser.add_argument('-v','--verbose', help='show more information about the execution.', action='store_true', default=False)
+
+    # Graphs types
     parser.add_argument('-p','--plot', help='Create a plot figure', action='store_true', default=False)
+
+    # Input options
     parser.add_argument('integers', help='a list of integers to plot', type=int, nargs='+')
+
+    # Customization options
     parser.add_argument('-f','--format', help="data representation with MATLAB syntax", default="")
     parser.add_argument('-t','--title', help='set the title of the figure', default="")
     parser.add_argument('--xlabel', help='set the x axis label in the figure', default="")
@@ -22,7 +29,11 @@ if __name__ == "__main__":
         print("Checking graph type")
     if(args.plot):
         plot = PlotGraph.PlotGraph(args.title, args.xlabel, args.ylabel)
+        if(args.verbose):
+            print("Drawing figure.")
         if(args.format != ""):
             plot.draw(args.integers, args.format)
         else:
             plot.draw(args.integers)
+    if(verbose):
+        print("Closing.")
