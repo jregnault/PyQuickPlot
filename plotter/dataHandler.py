5ffccde8d5ca9a955028fe87acd2c999b1eb9c45
Jérémie REGNAULT <jeremie.regnault.etu@univ-lille.fr>
2019-04-23 18:21:22 +0200

    add : readFromStdin

--- plotter/dataHandler.py
+++ plotter/dataHandler.py
@@ -3,8 +3,12 @@
 # 2019-03-25
 
 import csv
+import sys
 import numpy as np
 
-def importFromCSV(filename,separator):
+def readFromCSV(filename,separator):
         with open(filename, 'r') as file:
                 return np.array([[float(e) for e in row] for row in csv.reader(file, delimiter=separator, quoting=csv.QUOTE_NONE)])
+
+def readFromStdin():
+        return np.fromstring(sys.stdin.buffer.read(-1))
