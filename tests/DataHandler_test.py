515392396ed4403b132d265024a5041830b25bd4
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-05-01 15:16:51 +0200

    test : readFromStream with stdin input

--- tests/DataHandler_test.py
+++ tests/DataHandler_test.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import DataHandler as dh
 
@@ -5,5 +7,12 @@ class TestDataHandler(object):
     def test_readFromStreamWithFile(self):
         _expected = np.array([[1,2,3],[4,5,6]])
         _data = dh.DataHandler.readFromStream("./tests/simple_test.csv",',')
+        for exp, act in zip(_expected, _data):
+            assert list(exp) == list(act)
+    
+    def test_readFromStreamWithStdin(self):
+        sys.stdin = open("./tests/simple_test.csv")
+        _expected = np.array([[1,2,3],[4,5,6]])
+        _data = dh.DataHandler.readFromStream(None, "")
         for exp, act in zip(_expected, _data):
             assert list(exp) == list(act)
